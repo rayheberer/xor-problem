7cc22136ba6eacfa96a20d425c3ee5bed530c5c3
Ray Heberer <ray.heberer@gmail.com>
2018-04-09 11:16:03 -0700

    Create main script

---
+++ xor-problem.py
@@ -0,0 +1,19 @@
+import sys
+import model, data
+
+random_seed = 41
+dataset_size = 100000
+max_sequence_length = 50
+num_classes = 2
+
+def main(lstm_units):
+    data1, labels1, lengths1 = data.binary_equal_length(dataset_size, max_sequence_length)
+    LSTM = model.LSTM(lstm_units)
+    model.train_model(LSTM)
+    return LSTM
+
+if __name__ == "__main__":
+    
+    lstm_units = int(sys.argv[0])
+
+    trained_model = main(lstm_units)
